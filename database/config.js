5a6040613004029dc082577e00ae7b5735752d8b
jcest-ia <jc5est@gmail.com>
2026-05-26 20:35:13 -0500

    Remove deprecated options from mongoose connection
    Removed deprecated mongoose connection options.

--- database/config.js
+++ database/config.js
@@ -8,8 +8,6 @@ const dbConnection = async () => {
     try {
 
         await mongoose.connect(process.env.DB_CNN, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
         });
 
         console.log('DB Online');
